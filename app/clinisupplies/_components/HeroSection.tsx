9656a123432bf53d4a7b92e007dc1a5bd259d518
PraveenKumarTCP <tcp.praveenkumar@gmail.com>
2026-04-20 18:13:23 +0530

    Update hero content ui

--- app/clinisupplies/_components/HeroSection.tsx
+++ app/clinisupplies/_components/HeroSection.tsx
@@ -2,28 +2,20 @@ import DynamicVideoPlayer from "@/app/_components/DynamicVideoPlayer";
 
 export default function HeroSection() {
   return (
-    <section className="relative h-screen w-full overflow-hidden flex items-end justify-center text-white">
+    <section className="relative h-screen w-full overflow-hidden flex flex-col items-center justify-end text-white pb-10 md:pb-16" data-aos="fade-in">
       {/* 🎥 Background Video */}
       <DynamicVideoPlayer
         type="banner"
         className="absolute top-0 left-0 w-full h-full object-cover" />
       {/* <div className="absolute inset-0 bg-black/50"></div> */}
-      <div className="relative max-w-7xl text-center mb-16 px-6">
+      <div className="relative z-20 w-full container mx-auto px-6 text-left flex flex-col items-start">
         {/* HEADING */}
         <h1
           data-aos="fade-up"
-          className="text-xl md:text-3xl lg:text-[38px]  font-semibold mb-4 tracking-wide">
+          data-aos-delay="100"
+          className="text-xl md:text-3xl lg:text-[45px] leading-[1.2] font-semibold mb-8 tracking-tight max-w-3xl [text-shadow:4px_4px_8px_rgba(0,0,0,0.8)]">
           Livfria®, our next generation of collection devices
         </h1>
-
-        {/* SUBTEXT */}
-        <p
-          data-aos="fade-up"
-          className="text-[#FFFFFF] text-sm md:text-base lg:text-xl font-medium leading-relaxed">
-          Livfria® is our next-generation collection device, designed for
-          comfort, reliability, and ease of use, helping users feel confident
-          every day.
-        </p>
       </div>
     </section>
   );
