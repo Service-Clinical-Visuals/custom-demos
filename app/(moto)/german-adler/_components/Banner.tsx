9e6f1d127d4d50a9192cd39d74620dcb28dd87ef
tcpnandhini <tcp.nandhinidevi@gmail.com>
2026-09-03 13:30:15 +0530

    corrections

--- app/(moto)/german-adler/_components/Banner.tsx
+++ app/(moto)/german-adler/_components/Banner.tsx
@@ -7,20 +7,18 @@ export default function Banner() {
     <section className="relative w-full h-screen  overflow-hidden bg-black flex flex-col justify-end">
       {/* Background Video */}
       <div className="absolute inset-0 z-0">
-        <DynamicVideoPlayer 
-          type="banner" 
-          className="absolute inset-0 w-full h-full object-cover object-top" 
+        <DynamicVideoPlayer
+          type="banner"
+          className="absolute inset-0 w-full h-full object-cover object-fill"
         />
-       
       </div>
 
       {/* Content */}
       <div className="relative z-10 custom-container w-full pb-20 md:pb-32">
         <div className="max-w-[650px]" data-aos="fade-up" data-aos-delay="200">
           <h1 className="text-2xl md:text-3xl lg:text-[34px] !font-oxanium font-bold text-white leading-[1.3] tracking-wide ">
-            German Adler provides reliable lubricant
-            solutions for better performance and
-            longer equipment life.
+            German Adler provides reliable lubricant solutions for better
+            performance and longer equipment life.
           </h1>
         </div>
       </div>
