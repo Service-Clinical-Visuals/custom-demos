46dd3c39900d8855f3f1db1275fc75f8a6fb6a6d
tcpnandhini <tcp.nandhinidevi@gmail.com>
2026-09-03 16:08:55 +0530

    corrections

--- app/(moto)/german-adler/_components/Banner.tsx
+++ app/(moto)/german-adler/_components/Banner.tsx
@@ -9,7 +9,7 @@ export default function Banner() {
       <div className="absolute inset-0 z-0">
         <DynamicVideoPlayer
           type="banner"
-          className="absolute inset-0 w-full h-full object-cover object-fill"
+          className="absolute inset-0 w-full h-full object-cover lg:object-fill"
         />
       </div>
 
